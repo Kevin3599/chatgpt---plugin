0070b48848c8f8424d608dec331b565a3bc4102c
Kevin3599 <189818185@qq.com>
2024-10-27 21:49:42 -0700

    更改

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("Received message:", request);
   if (request.action === "callChatGPT") {
+    console.log("Calling ChatGPT with message:", request.message);
     fetch("http://localhost:5000/chat", {
       method: "POST",
       headers: {
@@ -7,9 +9,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       },
       body: JSON.stringify({ message: request.message })
     })
-    .then(response => response.json())
-    .then(data => sendResponse({ reply: data.reply }))
-    .catch(error => console.error("Error:", error));
+    .then(response => {
+      console.log("Received response from server");
+      return response.json();
+    })
+    .then(data => {
+      console.log("Received data:", data);
+      sendResponse({ reply: data.reply });
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
     return true;  // Will respond asynchronously.
   }
 });
